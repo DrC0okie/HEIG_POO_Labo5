77de14fa5b082ffb7a0f52a645f5f687e5ab42b6
Kevin Farine <kevin.farine@hotmail.com>
2022-11-09 20:51:06 +0100

    Header ajouté

--- Labo5/src/test/java/ch/heigvd/poo/labo5/tests/ProgramTest.java
+++ Labo5/src/test/java/ch/heigvd/poo/labo5/tests/ProgramTest.java
@@ -1,3 +1,12 @@
+/*−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−------------------------------------------------
+File name       : Program1.java
+Author(s)       : Kevin Farine, Timothée Van Hove
+Created         : 3 nov. 2022
+Description     : Program that execute +, - and * operations on matrices created
+                  with random values.
+Remark(s)       : This program automatically closes after displaying the result.
+JDK             : OpenJDK Runtime Environment Temurin-17.0.5+8 (build 17.0.5+8)
+−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−------------------------------------------------*/
 package ch.heigvd.poo.labo5.tests;
 
 import ch.heigvd.poo.labo5.Matrix.Matrix;
